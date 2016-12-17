cef283d85c9310320382f135dc5eacd13c3e2279
Rikkokiri <pilvi.rajala@gmail.com>
2016-12-17 01:11:47 -0500

    Move prospect response array to a separate file

---
+++ js/prospect-responses.js
@@ -0,0 +1,5 @@
+var responses = ["Naah bitch", "Don't even think about it",
+                  "You? Really?",
+                  "Hahaha... ha. \n No",
+                  "You'll get complimentary Carhart... IN HELL.",
+                  "Sorry, only women allowed"];
