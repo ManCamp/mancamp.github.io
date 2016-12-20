e1fdf9da2de0a77657c9771419da219e7dc9ed75
Rikkokiri <pilvi.rajala@gmail.com>
2016-12-19 22:13:01 -0500

    All the tiles added

--- game/js/application.js
+++ game/js/application.js
@@ -2,17 +2,19 @@
 window.requestAnimationFrame(function () {
   new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 });
-
-
+ d
     var imageList = [
-        "img/Kirk.png",
         "img/Ben.png",
+        "img/Kirk.png",
         "img/Carrie.png",
         "img/2Wendy.png",
+        "img/Brian.png"
         "img/Jenny.png",
         "img/Eden.png",
         "img/Banana.png",
-        "img/Monkey.png"
+        "img/Monkey.png",
+        "img/Zookeeper.png",
+        "img/Sign.png"
     ];
 
     for(var i = 0; i < imageList.length; i++ )
